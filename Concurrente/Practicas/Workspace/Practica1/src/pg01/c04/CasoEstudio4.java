6e4a133c580cdf6891d476a97218899698859023
Alejandro <aom1008@alu.ubu.es>
2021-03-08 11:42:47 +0100

    8/3/2021

---
+++ Concurrente/Practicas/Workspace/Practica1/src/pg01/c04/CasoEstudio4.java
@@ -0,0 +1,28 @@
+package pg01.c04;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CasoEstudio4 {
+	
+public static void main(String args[]) {
+		
+		Thread hilox = new Thread(new MyThread('X', 10, 1000));
+		Thread hiloo = new Thread(new MyThread('O', 10, 1000));
+		Thread hilo_ = new Thread(new MyThread('-', 10, 1000));
+		
+		//ExecutorService exec = Executors.newSingleThreadExecutor();
+		//ExecutorService exec = Executors.newFixedThreadPool(3);
+		ExecutorService exec = Executors.newSingleThreadExecutor();
+		
+		exec.execute(hilox);
+		exec.execute(hilo_);
+		exec.execute(hiloo);
+		
+		exec.shutdown();
+		
+		
+	}
+
+}
